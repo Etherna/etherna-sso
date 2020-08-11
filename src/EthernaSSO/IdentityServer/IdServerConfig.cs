221560d9b73cc9a0732fdfb20b11fc1f1ac69673
Mirko Da Corte <mirko@etherna.io>
2020-08-11 16:54:05 +0200

    Added configuration for etherna credit client

--- src/EthernaSSO/IdentityServer/IdServerConfig.cs
+++ src/EthernaSSO/IdentityServer/IdServerConfig.cs
@@ -9,7 +9,11 @@ namespace Etherna.SSOServer.IdentityServer
     public class IdServerConfig
     {
         // Fields.
+        private readonly string ethernaCreditBaseUrl;
+        private readonly string ethernaCreditSecret;
+
         private readonly string ethernaDappBaseUrl;
+
         private readonly string ethernaIndexBaseUrl;
         private readonly string ethernaIndexSecret;
 
@@ -18,6 +22,9 @@ namespace Etherna.SSOServer.IdentityServer
         {
             if (configuration is null)
                 throw new ArgumentNullException(nameof(configuration));
+
+            ethernaCreditBaseUrl = configuration["IdServer:Clients:EthernaCredit:BaseUrl"];
+            ethernaCreditSecret = configuration["IdServer:Clients:EthernaCredit:Secret"];
 
             ethernaDappBaseUrl = configuration["IdServer:Clients:EthernaDapp:BaseUrl"];
 
@@ -36,6 +43,31 @@ namespace Etherna.SSOServer.IdentityServer
         public IEnumerable<Client> Clients =>
             new List<Client>
             {
+                //credit
+                new Client
+                {
+                    ClientId = "ethernaCreditClientId",
+                    ClientName = "Etherna Credit",
+                    ClientSecrets = { new Secret(ethernaCreditSecret.Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = false,
+                    RequirePkce = true,
+
+                    // where to redirect to after login
+                    RedirectUris = { $"{ethernaCreditBaseUrl}/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { $"{ethernaCreditBaseUrl}/signout-callback-oidc" },
+
+                    AlwaysIncludeUserClaimsInIdToken = true,
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        "ether_accounts"
+                    }
+                },
+
                 //dapp
                 new Client
                 {
