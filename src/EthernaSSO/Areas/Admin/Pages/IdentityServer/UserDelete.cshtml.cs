e4b07782d3f83a5afee7fc63701283a944219dc3
Mirko Da Corte <mirko@etherna.io>
2021-09-23 22:25:07 +0200

    Implemented UserDelete page

--- src/EthernaSSO/Areas/Admin/Pages/IdentityServer/UserDelete.cshtml.cs
+++ src/EthernaSSO/Areas/Admin/Pages/IdentityServer/UserDelete.cshtml.cs
@@ -1,18 +1,42 @@
+using Etherna.SSOServer.Domain;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
+using System.Threading.Tasks;
 
 namespace Etherna.SSOServer.Areas.Admin.Pages.IdentityServer
 {
     public class UserDeleteModel : PageModel
     {
-        public string Id { get; private set; } = default!;
+        // Fields.
+        private readonly ISsoDbContext context;
 
-        public void OnGet(string id)
+        // Constructor.
+        public UserDeleteModel(
+            ISsoDbContext context)
+        {
+            this.context = context;
+        }
+
+        // Properties.
+        public string Id { get; private set; } = default!;
+        public string Username { get; private set; } = default!;
+
+        // Methods.
+        public async Task OnGetAsync(string id)
         {
             if (id is null)
                 throw new ArgumentNullException(nameof(id));
 
             Id = id;
+            var user = await context.Users.FindOneAsync(id);
+            Username = user.Username;
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(string id)
+        {
+            await context.Users.DeleteAsync(id);
+            return RedirectToPage("Users");
         }
     }
 }
