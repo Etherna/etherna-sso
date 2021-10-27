174738d3af6d5321bb86cb1f9166ed72e6e21b67
Mirko Da Corte <mirko@etherna.io>
2021-10-28 01:04:55 +0200

    Using EthernaRCL

--- src/EthernaSSO/Program.cs
+++ src/EthernaSSO/Program.cs
@@ -12,8 +12,8 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+using Etherna.RCL.Filters;
 using Etherna.SSOServer.Exceptions;
-using Etherna.SSOServer.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
