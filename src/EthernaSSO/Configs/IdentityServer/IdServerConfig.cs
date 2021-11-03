be28e70553de77f48c73da72f2cebb902dc42260
Mirko Da Corte <mirko@etherna.io>
2021-11-03 16:59:46 +0100

    Updated EthernaRCL

--- src/EthernaSSO/Configs/IdentityServer/IdServerConfig.cs
+++ src/EthernaSSO/Configs/IdentityServer/IdServerConfig.cs
@@ -12,8 +12,8 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+using Etherna.RCL.Exceptions;
 using Etherna.SSOServer.Domain.Models;
-using Etherna.SSOServer.Exceptions;
 using IdentityServer4;
 using IdentityServer4.Models;
 using Microsoft.Extensions.Configuration;
