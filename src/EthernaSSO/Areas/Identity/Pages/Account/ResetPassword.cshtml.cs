33eb7fd42d9869d5ee3168481cb107d4ca6fc861
Mirko Da Corte <mirko@etherna.io>
2026-05-05 17:41:21 +0200

    pass email to reset password page and update email input to hidden field

--- src/EthernaSSO/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ src/EthernaSSO/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -25,56 +25,46 @@ using System.Threading.Tasks;
 namespace Etherna.SSOServer.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
-    public class ResetPasswordModel : PageModel
+    public class ResetPasswordModel(UserManager<UserBase> userManager) : PageModel
     {
         // Models.
         public class InputModel
         {
             [Required]
             [EmailAddress]
-            public string Email { get; set; } = default!;
+            public string Email { get; set; } = null!;
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            public string Password { get; set; } = default!;
+            public string Password { get; set; } = null!;
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; } = default!;
+            public string ConfirmPassword { get; set; } = null!;
 
-            public string Code { get; set; } = default!;
-        }
-
-        // Fields.
-        private readonly UserManager<UserBase> _userManager;
-
-        // Constructor.
-        public ResetPasswordModel(UserManager<UserBase> userManager)
-        {
-            _userManager = userManager;
+            public string Code { get; set; } = null!;
         }
 
         // Properties.
         [BindProperty]
-        public InputModel Input { get; set; } = default!;
+        public InputModel Input { get; set; } = null!;
 
         // Methods.
-        public IActionResult OnGet(string? code = null)
+        public IActionResult OnGet(string? code = null, string? email = null)
         {
             if (code == null)
-            {
                 return BadRequest("A code must be supplied for password reset.");
-            }
-            else
+            if (email == null)
+                return BadRequest("An email must be supplied for password reset.");
+
+            Input = new InputModel
             {
-                Input = new InputModel
-                {
-                    Code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code))
-                };
-                return Page();
-            }
+                Code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code)),
+                Email = email
+            };
+            return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
@@ -84,14 +74,14 @@ namespace Etherna.SSOServer.Areas.Identity.Pages.Account
                 return Page();
             }
 
-            var user = await _userManager.FindByEmailAsync(Input.Email);
+            var user = await userManager.FindByEmailAsync(Input.Email);
             if (user == null)
             {
                 // Don't reveal that the user does not exist
                 return RedirectToPage("./ResetPasswordConfirmation");
             }
 
-            var result = await _userManager.ResetPasswordAsync(user, Input.Code, Input.Password);
+            var result = await userManager.ResetPasswordAsync(user, Input.Code, Input.Password);
             if (result.Succeeded)
             {
                 return RedirectToPage("./ResetPasswordConfirmation");
