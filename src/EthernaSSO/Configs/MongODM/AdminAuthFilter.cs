3898ea69088878c23416014c071fc51b053ec364
Mirko Da Corte <mirko@etherna.io>
2026-07-02 21:41:55 +0200

    Update package dependencies and improve dashboard authentication handling

--- src/EthernaSSO/Configs/MongODM/AdminAuthFilter.cs
+++ src/EthernaSSO/Configs/MongODM/AdminAuthFilter.cs
@@ -1,23 +1,23 @@
-﻿// Copyright 2021-present Etherna SA
+// Copyright 2021-present Etherna SA
 // This file is part of Etherna Sso.
-// 
+//
 // Etherna Sso is free software: you can redistribute it and/or modify it under the terms of the
 // GNU Affero General Public License as published by the Free Software Foundation,
 // either version 3 of the License, or (at your option) any later version.
-// 
+//
 // Etherna Sso is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License along with Etherna Sso.
 // If not, see <https://www.gnu.org/licenses/>.
 
+using Etherna.Authentication;
 using Etherna.MongODM.AspNetCore.UI.Auth.Filters;
 using Etherna.SSOServer.Domain.Models;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
-using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Etherna.SSOServer.Configs.MongODM
@@ -26,13 +26,14 @@ namespace Etherna.SSOServer.Configs.MongODM
     {
         public async Task<bool> AuthorizeAsync(HttpContext? context)
         {
-            if (context?.User is null)
+            if (context?.User.Identity?.IsAuthenticated != true)
                 return false;
-            var userManager = context.RequestServices.GetService<UserManager<UserBase>>()!;
 
-            var user = await userManager.GetUserAsync(context.User) ?? throw new InvalidOperationException();
+            // Verify role on claims, without db reads: keeps the dashboard reachable while a migration locks the db context.
+            var ethernaOidcClient = context.RequestServices.GetRequiredService<IEthernaOpenIdConnectClient>();
+            var roles = await ethernaOidcClient.TryGetRolesAsync();
 
-            return await userManager.IsInRoleAsync(user, Role.AdministratorName);
+            return roles?.Contains(Role.NormalizeName(Role.AdministratorName)) == true;
         }
     }
 }
